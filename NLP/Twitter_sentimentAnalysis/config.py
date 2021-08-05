b9d027f9d455c8d6ee4e9215e674be15c10c4fe1
steffanp98 <steffan.phillips@hotmail.com>
2021-08-05 18:05:58 +0100

    gitignore

---
+++ NLP/Twitter_sentimentAnalysis/config.py
@@ -0,0 +1,5 @@
+#API keys 
+consumerKey = 'ZgsEQ0PM2euRdJHyfrMIGdWBM'
+consumerSecret = 'rcO5BwZPc7SEeXCAAxSm5CWXOpXiixvAlRqdNy7E7Qz64qkWGG'
+accessToken = '1229731149414531072-HDN1aMjvmnlw2hsVuEEgUaBPxNrqvP'
+accessTokenSecret = 'vRkVl95HuslQS0X7hFHZpfQwGk4MydT2IgpDhiNSKbZOn'
